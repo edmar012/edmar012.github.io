5e173fc91ac984f5d66e5f9f679aadc1135451c2
edmar012 <155861109+edmar012@users.noreply.github.com>
2024-01-07 11:25:30 +0800

    Add files via upload

---
+++ data/CatalabanIsland_4.js
@@ -0,0 +1,1 @@
+var json_CatalabanIsland_4 = {"type":"FeatureCollection","name":"CatalabanIsland_4","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"Island":"Catalaban "},"geometry":{"type":"Point","coordinates":[125.465641640853832,11.856711884825472]}}]}
